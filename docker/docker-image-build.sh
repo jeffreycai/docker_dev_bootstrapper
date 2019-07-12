b11f97051d1d6f85d37cdfd8ce99101e11c1a7a7
jeffreycai <jeffrey.cai@qantasloyalty.com>
2019-07-12 14:34:53 +1000

    Add enforce build image option

--- docker/docker-image-build.sh
+++ docker/docker-image-build.sh
@@ -1,7 +1,7 @@
 # build docker image if not yet
 img_built=$(docker images | grep ${APP_NAME})
-if [ -z "${img_built}" ]; then
-  log "Image ${APP_NAME} not yet built. Build now.."
+if [ -z "${img_built}" ] || [ ! -z "${ENFORCE_IMAGE_BUILD}" ]; then
+  log "Image ${APP_NAME} not yet built or enforced to be built. Build now.."
   cd $BASEDIR
   docker build . -t ${APP_NAME} -f $BASEDIR/docker/Dockerfile
 else
--- docker/functions.sh
+++ docker/functions.sh
@@ -9,3 +9,16 @@ header() {
   echo "* ${msg} *"
   echo "======================"
 }
+
+# Handle options
+while getopts f FLAG; do
+  case $FLAG in
+    f)
+      export ENFORCE_IMAGE_BUILD=1
+      ;;
+    \?) #unrecognized option - show help
+      echo "Option -$OPTARG not allowed."
+      exit 1
+      ;;
+  esac
+done
